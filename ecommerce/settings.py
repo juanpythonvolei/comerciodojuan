e920f41389832e8445de7222cb7037bab117e92c
juanpythonvolei <juanzsalca@outlook.com>
2024-09-09 14:42:37 -0300

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = f"{os.getenv('SECRET_KEY')}"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1",""]
+ALLOWED_HOSTS = ["127.0.0.1","comerciodojuan.onrender.com"]
 
 
 # Application definition
